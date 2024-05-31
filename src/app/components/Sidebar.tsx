858def7eb9baa96d1aec851f21dca0fb8f52f1cd
Carlos Anriquez <carlos.anriquez@devsavant.ai>
2024-05-31 00:20:28 -0300

    adds mini profile to side bar component

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,15 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
 "use client"
 
 import { signIn, signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React from 'react'
 import { FaXTwitter } from "react-icons/fa6";
-import { HiHome } from "react-icons/hi";
+import { HiHome, HiDotsHorizontal } from "react-icons/hi";
 
 export default function Sidebar() {
   const { data: session } = useSession();
   return (
-    <div className='flex flex-col gap-4 p-3'>
+    <div className='flex flex-col p-3 justify-between h-screen'>
+      <div className='flex flex-col gap-4 p-3'>
       <Link href='/'>
         <FaXTwitter className='w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200' />
       </Link>
@@ -19,16 +21,29 @@ export default function Sidebar() {
       </Link>
       { session 
         ? <button className='bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline'
-          onClick={() => signOut()}
-          >
+        onClick={() => signOut()}
+        >
           Sign Out
         </button>
         :<button className='bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline'
-          onClick={() => signIn()}
-          >
+        onClick={() => signIn()}
+        >
           Sign In
         </button>
       }
       </div>
+      {
+        session && (
+          <div className='text-gray-700 text-sm flex items-center cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200'>
+            <img src={session?.user?.image ?? ''} alt='usr-img' className='h-10 w-10 rounded-full xl:mr-2'/>
+            <div className='hidden xl:inline'>
+              <h4 className='font-bold'>{session?.user?.name}</h4>
+              <p className='text-gray-500'>@{session?.user?.username}</p>
+            </div>
+            <HiDotsHorizontal className='h-5 xl:ml-8 hidden xl:inline' />
+          </div>
+        )
+      }
+    </div>
   )
 }
